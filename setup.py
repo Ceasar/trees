fac15d43417fc9c9efb8df1cd59233786a2f1426
Adel Qalieh <aqalieh95@gmail.com>
2015-12-22 21:38:14 -0500

    Bump version and add classifiers

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
